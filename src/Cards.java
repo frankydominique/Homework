5b7bf9e4c73a98ac74804272d8411c3e63c999ce
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2019-09-26 08:52:10 -0400

    Created Card class and added fields

---
+++ src/Cards.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Franceska
+ *
+ */
+public class Cards {
+
+	private int suit;
+	private int rank;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
