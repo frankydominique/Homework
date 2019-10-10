7647464ea15381f6e2a211391505353bb1da7472
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2019-10-09 22:50:47 -0400

    finished test class

--- src/TestClass.java
+++ src/TestClass.java
@@ -20,9 +20,14 @@ public class TestClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test1 = (int)(Math.random() * 10);
-		test2 = (int)(Math.random() * 10);
-		testCards();
+		for(int x = 0; x < 10; x++)
+		{
+			test1 = (int)(Math.random() * 10);
+			test2 = (int)(Math.random() * 10);
+			testCards();
+			testDeck();
+		}
+		
 	}
 	
 	public static void testCards()
@@ -31,12 +36,18 @@ public class TestClass {
 		Cards testing1 = new Cards("Hearts", "Jack");
 		Cards testing2 = new Cards("Hearts", test1);
 		Cards testing3 = new Cards(test1, "Jack");
+		Cards testing4 = new Cards(test2, test1);
+		Cards testing5 = new Cards(test1, null);
+		Cards testing6 = new Cards(null, test2);
 		
-		Cards[] testSet = {testing, testing1, testing2, testing3};
+		Cards[] testSet = {testing, testing1, testing2, testing3, testing4, testing5, testing6};
+		
+		CardComparator comp = new CardComparator();
 		
 		for(Cards x: testSet)
 		{
-			System.out.print(x.toString() + ", compareTo(testing): " + x.compareTo(testing) + ", equals: " + x.equals(testing));
+			System.out.println(x.toString() + ", compareTo(testing): " + x.compareTo(testing) + ", equals: " + x.equals(testing));
+			System.out.println(comp.compare(x, testing));
 			System.out.println();
 		}
 	}
@@ -46,19 +57,33 @@ public class TestClass {
 		Deck test = new Deck();
 		Deck testing = new Deck(false);
 		Deck testing2 = new Deck(false);
+		Deck testing3 = new Deck(true);
+		Deck testing4 = new Deck(4);
+		Deck testing5 = new Deck(0);
+		Deck testing6 = new Deck();
 		
-		Deck[] testSet = {test, testing, testing2};
+		Deck[] testSet = {test, testing, testing2, testing3, testing4, testing5, testing6};
 		
 		for(int i = 0; i < testSet.length; i++)
 		{
-			System.out.print(testSet[i].toString() + ", equals: " + testSet[i].equals(testing) + ", picked Card: " + testSet[i].pick());
+			System.out.println("Testing toString(), equals(), and pick(): ");
+			System.out.println(testSet[i].toString() + ", equals: " + testSet[i].equals(testing) + ", picked Card: " + testSet[i].pick());
 			
+			System.out.println("Testing deal(): ");
 			Deck[] temp = testSet[i].deal(test1, test2);
+			for(Deck x: temp)
+				System.out.println("hand: " + x);
 			System.out.println();
 			
+			System.out.println("Testing mergeSort(): ");
 			testSet[i].mergeSort();
-			
 			System.out.println(testSet[i].toString());
+			
+			System.out.println("Testing selectionSort()": );
+			testSet[i].shuffle();
+			testSet[i].selectionSort();
+			for(Deck x: testSet)
+				System.out.println("selectionSort shuffle: " + x);
 		}
 		
 		
