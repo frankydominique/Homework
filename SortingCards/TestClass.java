cb1704ad10f1cf40b6cf89c8b4586ba23644e201
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2019-10-23 14:27:06 -0400

    Renamed src to SortingCards

--- SortingCards/TestClass.java
+++ SortingCards/TestClass.java
@@ -11,13 +11,6 @@ public class TestClass {
 	/**
 	 * 
 	 */
-	public TestClass() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int x = 0; x < 10; x++)
