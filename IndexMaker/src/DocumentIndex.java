1b8b758e28b91c841e2df673c2f037ee666cf240
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2019-11-02 17:39:24 -0400

    Added methods to DocumentIndex

--- IndexMaker/src/DocumentIndex.java
+++ IndexMaker/src/DocumentIndex.java
@@ -4,17 +4,46 @@
  */
 import java.util.*;
 
-public class DocumentIndex implements List<IndexEntry>{
+public class DocumentIndex{
 
-	private List<IndexEntry> list;
+	private TreeMap<String, IndexEntry> indices;
 	
 	public DocumentIndex()
 	{
-		list = new LinkedList<IndexEntry>();
+		indices = new TreeMap<String, IndexEntry>();
 	}
 	
+	/*
+	 * Adds num to the IndexEntry for word using IndexEntry's add(num) method
+	 * If indices doesn't contain this word, adds key to indices
+	 * 
+	 *@param word - word found in document that has an index
+	 *@param num - the newly found index of the word 
+	 */
 	public void addWord(String word, int num)
 	{
-		
+		IndexEntry temp;
+		//do i need to check if indices is empty? what happens if i just do contains?
+		if(indices.isEmpty() || !indices.containsKey(word))
+			indices.put(word, new IndexEntry(word));
+
+		temp = indices.get(word);
+		temp.add(num);
+	}
+	
+	/*
+	 * Extracts all the words from str and for each word calls addWord(word, num)
+	 * 
+	 * @param str - line from a certain document
+	 * @param num - line number from document
+	 */
+	public void addAllWords(String str, int num)
+	{
+		String temp = str;
+		for(int i = 0; i < str.length(); i = temp.indexOf(" " + 1))
+		{
+			temp = temp.substring(i, temp.indexOf(" "));
+			addWord(temp, num);
+		}
 	}
 }
