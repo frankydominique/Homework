5d5b5803a3d0a168268459946fc8f722e6029b5a
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2019-12-30 22:34:18 -0500

    Finished CardNode class

--- SortingCards/CardNode.java
+++ SortingCards/CardNode.java
@@ -13,12 +13,55 @@ public class CardNode {
 	public CardNode(Card card)
 	{
 		this.card = card;
+		next = null;
+		previous = null;
 	}
 	
 	//do you need to go through list until a card matches previous? actually using .equals?
-	public CardNode(Card card, Card previous)
+	public CardNode(Card card, CardNode previous)
 	{
-		this.card = card;
-		
+		if(previous != null)
+		{
+			this.card = card;
+			this.previous = previous;
+			this.next = previous.getNext();
+			previous.getNext().setPrevious(this);
+			previous.setNext(this);
+		}
+	}
+	
+	public Card getCard()
+	{
+		return card;
+	}
+	
+	public CardNode getNext()
+	{
+		return next;
+	}
+	
+	public CardNode getPrevious()
+	{
+		return previous;
+	}
+	
+	public int getNodeNum()
+	{
+		return nodeNum;
+	}
+	
+	public void setNext(CardNode next)
+	{
+		this.next = next;
+	}
+	
+	public void setPrevious(CardNode previous)
+	{
+		this.previous = previous;
+	}
+	
+	public void setNodeNum(int nodeNum)
+	{
+		this.nodeNum = nodeNum;
 	}
 }
