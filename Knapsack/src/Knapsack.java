d4b95374f7b8bb525a196fc7cb817b23d015e009
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2020-01-22 23:32:11 -0500

    Fixed print statements and knapsackSum

--- Knapsack/src/Knapsack.java
+++ Knapsack/src/Knapsack.java
@@ -11,6 +11,8 @@ public class Knapsack {
 	private static File[] files = new File[3];
 	private static int[] limits = new int[10];
 	private static int limitPos = 0;
+	private static int[][] values = new int[10][10];
+	private static int valuesXPos = 0;
 	private static ArrayList<List<Integer>> lists = new ArrayList<List<Integer>>();
 	private static File output = new File("knapsack.txt");
 	
@@ -19,17 +21,31 @@ public class Knapsack {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//puts in the test file
+		
 		File test = new File("Tester.txt");
+		
+		//reads the test file and divides the test file into the smaller files to be used
 		readBigFile(test);
 		
-		for(int x = 0; x < 3; x++)
-			System.out.println(files[x].getName());
-		
+		//adds the limits and values to the corresponding lists
 		for(File x: files)
-			readFileLimit(x);
+			readLimsAndVals(x);
 		
-		for(int x: limits)
-			System.out.println(x);
+		
+		
+		//do knapsack - knapsack is working
+		for(int i = 0; i < files.length; i++)
+		{
+			System.out.println(knapsackSum(values[i], values[i].length - 1, limits[i], lists.get(i)));
+		}
+		printFile();
+		
+		/*ArrayList<Integer> test = new ArrayList<Integer>();
+		int[] set = {1, 2, 3, 4, 5};
+		System.out.println(knapsackSum(set, set.length - 1, 6, test));
+		for(Integer x: test)
+			System.out.println(x);*/
 	}
 	
 	public static int knapsack(int[] w, int n, int limit)
@@ -54,28 +70,33 @@ public class Knapsack {
 			return 0;
 		else if (w[n] > limit)
 		{
-			int val = knapsack(w, n-1, limit);
+			int val = knapsackSum(w, n-1, limit, list);
 			list.add(val);
 			return val;
 		}
 		else
 		{
-			int withLast = knapsack(w, n-1, limit);
-			int withoutLast = w[n] + knapsack(w, n-1, limit - w[n]);
-			if(withLast > withoutLast)
+			//int withoutLast = knapsack(w, n-1, limit);
+			List<Integer> withoutLastList = new ArrayList<Integer>();
+			List<Integer> withLastList = new ArrayList<Integer>();
+			withLastList.add(w[n]);
+			int withoutLast = knapsackSum(w, n-1, limit, withoutLastList);
+			int withLast = w[n] + knapsackSum(w, n-1, limit - w[n], withLastList);
+			
+			if(withoutLast > withLast)
 			{
-				list.add(withLast);
-				return withLast;
+				list.add(withoutLast);
+				return withoutLast;
 			}
-			list.add(withoutLast);
-			return withoutLast;
+			list.addAll(withLastList);
+			return withLast;
 		}
 	}
 
+	//gets big file and breaks the different files and places it into files
 	public static void readBigFile(File pathname)
 	{
 		File file = pathname;
-		StringBuffer strBuffer = new StringBuffer((int)file.length());
 		
 		try
 		{
@@ -98,7 +119,8 @@ public class Knapsack {
 		}
 	}
 	
-	public static void readFileLimit(File pathname)
+	//reads the limit of each file
+	public static void readLimsAndVals(File pathname)
 	{
 		File file = pathname;
 		StringBuffer strBuffer = new StringBuffer((int)file.length());
@@ -113,12 +135,23 @@ public class Knapsack {
 			while((ch = input.read()) != -1)
 				strBuffer.append((char)ch);
 			
-			input.close();
-			
 			Scanner scanner = new Scanner(file);
 			
 			limits[limitPos] = Integer.parseInt(scanner.nextLine());
 			limitPos++;
+			
+			int pos = 0;
+			while(scanner.hasNextLine())
+			{
+				values[valuesXPos][pos] = scanner.nextInt();
+				pos++;
+			}
+			lists.add(new ArrayList<Integer>(pos));
+			valuesXPos++;
+			
+			scanner.close();
+			input.close();
+			
 		}
 		catch (IOException ex)
 		{
@@ -126,7 +159,8 @@ public class Knapsack {
 		}
 	}
 	
-	public void printFile()
+	//prints all the top values into the output file
+	public static void printFile()
 	{
 		Writer append2 = null;
 		
@@ -143,11 +177,12 @@ public class Knapsack {
 		
 		for(int i = 0; i < files.length; i++)
 		{
-			toPrint.print(files[i] + "\t" + limits[i] + "\t"); //+ printNums(files[i]));
+			toPrint.print(files[i] + "\t" + limits[i] + "\t" + printNums(lists.get(i))); //+ printNums(files[i]));
 			toPrint.println();
 			
 			for(Integer x: lists.get(i))
-				toPrint.print(x + "pound melon");
+				if(x != 0)
+					toPrint.print(x + "pound melon \n");
 			
 			toPrint.println();
 			toPrint.println();
@@ -157,12 +192,16 @@ public class Knapsack {
 		
 	}
 	
-	public String printNums(List<Integer> list)
+	//gathers the nums to be printed from the list generated of the values that add up
+	public static String printNums(List<Integer> list)
 	{
 		StringBuffer nums = new StringBuffer();
 		
 		for(Integer x: list)
-			nums.append(x + ", ");
+		{
+			if(x != 0)
+				nums.append(x + ", ");
+		}
 		
 		return nums.toString();
 	}
