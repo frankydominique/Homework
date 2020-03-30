84786063e01e73ef9bfd6ae988bfd21bdc61666e
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2020-03-29 22:10:10 -0400

    used java.util.PriorityQueue

--- PriorityMessageQueue/src/Simulation.java
+++ PriorityMessageQueue/src/Simulation.java
@@ -5,27 +5,10 @@ public class Simulation {
 	public static void main(String[] args)
 	{
 		//testMyPriorityQueue();
-		PriorityQueue<Message> pq = new PriorityQueue<Message>(5, new MessageComparator());
-		Message[] removed = new Message[50];
-		int pos = 0;
 		
-		//change count to greater than 30
-		//you won't see messages that have not been processed yet because it takes four minutes to process them
-		for(int i = 0; i < removed.length; i++)
-		{
-			Message newMsg = new Message((int)(Math.random() * 5), i, "" + i);
-			pq.add(newMsg);
-			
-			if(i >= 4)
-			{
-				removed[pos] =  pq.remove();
-				pos++;
-			}
-		}
+		System.out.println("testing the real Priority Queue...");
 		
-		for(Message msg: removed)
-			System.out.println(msg);
-		
+		testPriorityQueue();
 	}
 	
 	public static void testMyPriorityQueue()
@@ -56,5 +39,44 @@ public class Simulation {
 		
 		for(double times: mpq.getAverageWaitingTimes())
 			System.out.println("average wait time: " + times);
+	}
+	
+	public static void testPriorityQueue()
+	{
+		PriorityQueue<Message> pq = new PriorityQueue<Message>(5, new MessageComparator());
+		Message[] removed = new Message[50];
+		int pos = 0;
+		
+		for(int i = 0; i < removed.length; i++)
+		{
+			Message newMsg = new Message((int)(Math.random() * 5), i, "" + i);
+			pq.add(newMsg);
+			
+			if(i >= 4)
+			{
+				Message msg =  pq.remove();
+				Message[] temp = new Message[10];
+				int index = 0;
+				
+				//need to make an array that stores all the values that don't work before adding them back in
+				while(i - msg.getArrivalTime() < 4)
+				{
+					temp[index] = msg;
+					msg = pq.remove();
+					index++;
+				}
+				removed[pos] = msg;
+				for(Message x: temp)
+				{
+					if(x != null) pq.add(x);
+				}
+				
+				pos++;
+			}
+			
+		}
+		
+		for(Message msg: removed)
+			System.out.println(msg);
 	}
 }
